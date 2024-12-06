8cf1ef8a632aa56c9a662afbee699ce28c84afb3
DavletgaraevAyaz <ayaz.davletgaraev2017@mail.ru>
2024-12-06 13:40:33 +0300

    del

--- Classes/CurrentUser.cs
+++
@@ -1,5 +0,0 @@
-﻿public static class CurrentUser
-{
-    public static int ID { get; set; }
-    public static string Name { get; set; }
-}
--- Classes/TrainingViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FitnessOsnova_Kam_Dav.Classes
-{
-    public class TrainingViewModel
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-    }
-}
